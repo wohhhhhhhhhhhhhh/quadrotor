99d2ca72122452880aad73a0c26421c29f5dda9a
woh <sunny159357@qq.com>
2026-05-16 08:54:29 +0800

    更改PID参数

--- Chariot/inc/para_gimbal.hpp
+++ Chariot/inc/para_gimbal.hpp
@@ -39,16 +39,16 @@
 #define YAW_INNER_LOWPASS_FILTER_PARA 0.8f     // 1.0f
 // 云台Pitch电机(DM4310)
 #define PITCH_OUTER_KP                  40.0f // 外环 15.0
-#define PITCH_OUTER_KI                  0.1f  // 0.0
+#define PITCH_OUTER_KI                  1.0f  // 0.0
 #define PITCH_OUTER_KD                  80.0f // 0.0
 #define PITCH_OUTER_OUT_LIMIT           10.0f // 18.0
 #define PITCH_OUTER_IOUT_LIMIT          0.5f  // 0.1
 #define PITCH_INNER_KP                  0.65f // 内环 0.35
-#define PITCH_INNER_KI                  0.0f  // 0.0
-#define PITCH_INNER_KD                  0.05f // 0.05
+#define PITCH_INNER_KI                  0.005f  // 0.0
+#define PITCH_INNER_KD                  0.0f // 0.05
 #define PITCH_INNER_OUT_LIMIT           10.0f // 5.0
-#define PITCH_INNER_IOUT_LIMIT          0.0f  // 0.0
-#define PITCH_INNER_LOWPASS_FILTER_PARA 0.9f  // 1.0
+#define PITCH_INNER_IOUT_LIMIT          0.5f  // 0.0
+#define PITCH_INNER_LOWPASS_FILTER_PARA 1.0f  // 1.0
 // 重力补偿前馈（Nm）
 #define PITCH_GRAVITY_COMPENSATE 0.0f
 // 摩擦轮(M3508)
